03e9939997b2c28c4c19e7bf8e48cb3b85296319
sdbang <seungdae.bang@archipin.com>
2022-10-07 18:30:32 +0900

    first commit

---
+++ src/main/java/com/archipin/ludiutils/service/Test.java
@@ -0,0 +1,11 @@
+package com.archipin.ludiutils.service;
+
+public class Test {
+    public static String getString() {
+        return "test";
+    }
+
+    public String getName() {
+        return "dennis";
+    }
+}
