b14db2e68aed3bd6fe363670971c3e4632ee98a7
sdbang-archipin <seungdae.bang@archipin.com>
2022-10-12 14:56:32 +0900

    Add getter and setter for Level class

--- src/main/java/com/archipin/ludiutils/domain/Level.java
+++ src/main/java/com/archipin/ludiutils/domain/Level.java
@@ -2,12 +2,24 @@ package com.archipin.ludiutils.domain;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Level {
     private Integer major;
     private Integer minor;
+
+
+    public Integer getMajor() {
+        return major;
+    }
+
+    public void setMajor(Integer major) {
+        this.major = major;
+    }
+
+    public Integer getMinor() {
+        return minor;
+    }
+
+    public void setMinor(Integer minor) {
+        this.minor = minor;
+    }
 }
