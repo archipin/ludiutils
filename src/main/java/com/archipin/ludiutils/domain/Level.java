c05db67bd48a10982a167c63fbae23957b7bab5a
sdbang-archipin <seungdae.bang@archipin.com>
2022-10-12 12:32:04 +0900

    Add level type

---
+++ src/main/java/com/archipin/ludiutils/domain/Level.java
@@ -0,0 +1,13 @@
+package com.archipin.ludiutils.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Level {
+    private Integer major;
+    private Integer minor;
+}
