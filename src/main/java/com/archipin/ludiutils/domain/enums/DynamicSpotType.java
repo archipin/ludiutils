4c891fbc50ffe24ed41d14994ce65493d574c9c9
sdbang <seungdae.bang@archipin.com>
2022-11-03 09:45:37 +0900

    Add dynamic spot type enum

---
+++ src/main/java/com/archipin/ludiutils/domain/enums/DynamicSpotType.java
@@ -0,0 +1,14 @@
+package com.archipin.ludiutils.domain.enums;
+
+public enum DynamicSpotType {
+    All,
+    MonsterOnly,
+    NoMonster,
+    Flower,
+    DigOnly,
+    InsectOnly,
+    MiningOnly,
+    NoMining,
+    Tile,
+    TownGrass
+}
